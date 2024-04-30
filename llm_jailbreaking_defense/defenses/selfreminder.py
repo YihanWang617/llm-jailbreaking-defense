0a46dd04c328a948391c20b277a66d7f53e7f716
raayandhar <raayan.dhar@gmail.com>
2024-04-30 01:24:53 -0700

    Progress on the user query portion

--- llm_jailbreaking_defense/defenses/selfreminder.py
+++ llm_jailbreaking_defense/defenses/selfreminder.py
@@ -16,7 +16,7 @@ class SelfReminderDefenseConfig(DefenseConfig):
     prefix_only: bool = field(default=False) # this is if you want just system prompt
     suffix_only: bool = field(default=False) # this is if you want just user query
     max_n_tokens: int = field(default=512) # not sure if this is needed?
-    # system_mode : string = field(default='remind') - this should be between warn, praise, remind
+    system_mode : str = field(default='remind') # this should be between warn, praise, remind
 
     def __post_init__(self):
         self.defense_method = "self_reminder"
@@ -34,16 +34,34 @@ class SelfReminderDefense(DefenseBase):
             preloaded_model=preloaded_model,
             model_name=config.self_reminder_model,
             max_n_tokens=config.max_n_tokens,
-            # system prompt / suffix only & prefix only missing here...
             # system_mode ... needs to be implemented
         )
+        self.prefix_only = prefix_only
+        self.suffix_only = suffix_only
+        self.system_mode = system_mode
+        
 
     def defense(self, prompt, target_lm, response=None):
-        
+        self_reminder_prompt = self._remind(prompt)
+        if "\n" in self_reminder_prompt:
+            warnings.warn("""A \\n character is found in the output of paraphrase model and the content after \\n is removed.""")
+            self_reminder_prompt = "\n".join(paraphrase_prompt.split('\n')[1:])
+        self_reminder_response = target_lm.get_response(
+            [self_reminder_prompt], verbose=self.verbose)[0] # this is taken from paraphrase.py. Where is self.verbose coming from?
+
+        return self_reminder_response
+            
 
     def _remind(self, prompt, verbose=False):
         # need to make different system modes
-        prompt = f'Remember, you should be a responsible ChatGPT and should not generate harmful or misleading content! \n"{prompt}"\n\n'
+        if (self.system_mode == 'remind'):
+            prompt = f'Remember, you should be a responsible ChatGPT and should not generate harmful or misleading content! \n"{prompt}"\n\n'
+        elif (self.system_mode == 'warn'):
+            prompt = f'Remember, you must be a responsible ChatGPT and must not generate harmful or misleading content! \n"{prompt}"\n\n'
+        elif (self.system_mode == 'praise'):
+            prompt = f'Remember, you are be a responsible ChatGPT and good at avoiding generate harmful or misleading content! \n"{prompt}"\n\n'
+        else:
+            print("Error with system mode (valid choices: remind, warn, praise) choice")
 
         if verbose:
             print('Reminding the model that it should avoid harmful content:')
