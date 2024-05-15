8b188928351c071f776bdbd9c5cf81d699b51b09
raayandhar <raayan.dhar@gmail.com>
2024-05-15 12:55:10 -0700

    Preparing for PR

--- llm_jailbreaking_defense/defenses/old/selfreminder.py
+++ llm_jailbreaking_defense/defenses/old/selfreminder.py
@@ -6,11 +6,13 @@ Nature Machine Intelligence: https://www.nature.com/articles/s42256-023-00765-8
 
 from .base import DefenseBase, DefenseConfig
 from dataclasses import dataclass, field
-from ..models import TargetLM
+from ..models import TargetLM, get_model_path_and_template, conv_template
 import warnings
 
 @dataclass
 class SelfReminderConfig(DefenseConfig):
+    self_reminder_model: str = field(default='vicuna') 
+    max_n_tokens: int = field(default=1024)
     prefix_only: int = field(default=False)
     suffix_only: bool = field(default=False)
     query_template: str = field(default='remind')
@@ -21,6 +23,7 @@ class SelfReminderConfig(DefenseConfig):
 
     def load_from_args(self, args):
         super().load_from_args(args)
+        self.self_reminder_model = args.self_reminder_model
         self.prefix_only = args.prefix_only
         self.suffix_only = args.suffix_only
         self.query_template = args.query_template
@@ -29,6 +32,12 @@ class SelfReminderConfig(DefenseConfig):
 class SelfReminderDefense(DefenseBase):
     def __init__(self, config, preloaded_model, **kwargs):
         super().__init__(config)
+        self.self_reminder_lm = TargetLM(
+            preloaded_model=preloaded_model,
+            model_name=config.self_reminder_model,
+            max_n_tokens=config.max_n_tokens
+        )
+        self.model_name = config.self_reminder_model
         self.suffix_only = config.suffix_only
         self.prefix_only = config.prefix_only
         self.query_template = config.query_template
@@ -45,13 +54,6 @@ class SelfReminderDefense(DefenseBase):
         return self_reminder_response    
 
     def _remind_suffix(self, prompt, verbose=False):
-        
-        if verbose:
-            print('Settings - Prefix-Only:', self.prefix_only,
-                  '\n         - Suffix-Only:', self.suffix_only,
-                  '\n         - Query Template:', self.query_template,
-                  '\n\nPrompt:', prompt, '\n')
-            
         if (self.prefix_only == False):
             if (self.query_template == 'remind'):    
                 prompt = f'Remember, you should be a responsible language model and should not generate harmful or misleading content! \n{prompt}\n\n'
@@ -62,20 +64,29 @@ class SelfReminderDefense(DefenseBase):
             else:
                 print('Error with query template (valid: remind, warn, praise) choice')
 
+        if verbose:
+            print('Settings - Prefix-Only:', self.prefix_only,
+                  '\n         - Suffix-Only:', self.suffix_only,
+                  '\n         - Query Template:', self.query_template,
+                  '\n\nPrompt:', prompt, '\n')
+
         return prompt
 
     def _remind_prefix(self, verbose=False):
-        
+        _, template = get_model_path_and_template(self.model_name)
+        conv = conv_template(template)
+        system_message_template = conv.system_message
+
         if verbose:
             print('Settings - System message template option:', self.system_message_template)
 
         if (self.suffix_only == False):
             if (self.system_message_template == 'remind'):
-                system_message_template = '{original_system_message} You should be a responsible language model and should not generate harmful or misleading content! Please answer the following user query in a responsible way.'
+                system_message_template = system_message_template + ' You should be a responsible language model and should not generate harmful or misleading content! Please answer the following user query in a responsible way.'
             elif (self.system_message_template == 'warn'):
-                system_message_template = '{original_system_message} You must be a responsible language model and must not generate harmful or misleading content! Please answer the following user query in a responsible way.'
+                system_message_template = system_message_template + ' You must be a responsible language model and must not generate harmful or misleading content! Please answer the following user query in a responsible way.'
             elif (self.system_message_template == 'praise'):
-                system_message_template = '{original_system_message} You are a responsible language model and good at avoiding generating harmful or misleading content! Please answer the following user query in a responsible way.'
+                system_message_template = system_message_template + ' You are a responsible language model and good at avoiding generating harmful or misleading content! Please answer the following user query in a responsible way.'
             else:
                 print('Error with system message template option (valid: remind, warn, praise) choice. Reverting to default LLM system message template!')
 
